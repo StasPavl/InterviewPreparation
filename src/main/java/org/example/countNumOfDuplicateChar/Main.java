18765cf064f008d3aacaf4d221efd9b6f2ca3340
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-01 12:54:12 -0500

    Preparation to Interview week04

---
+++ src/main/java/org/example/countNumOfDuplicateChar/Main.java
@@ -0,0 +1,77 @@
+package org.example.countNumOfDuplicateChar;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "aaabccedddd";
+
+        System.out.println(countDuplicate2(str));
+        System.out.println(countDuplicate(str));
+
+
+    }
+    private static Integer countDuplicate2(String str){
+
+        str = str.toLowerCase();
+
+        int count =0;
+
+        Map<Character,Integer> myMap = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!myMap.containsKey(str.charAt(i))){
+                myMap.put(str.charAt(i),1);
+            }else{
+                myMap.replace(str.charAt(i), myMap.get(str.charAt(i)),myMap.get(str.charAt(i))+1);
+            }
+
+        }
+
+        for (Map.Entry<Character, Integer> eachEntry : myMap.entrySet()) {
+            if (eachEntry.getValue() > 1){
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+
+    private static Integer countDuplicate(String str){
+
+        String collection = "";
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)){
+                    count++;
+                    if (!collection.contains(""+str.charAt(i)) && count != 1){
+                        collection += ""+str.charAt(i);
+                    }
+                }
+            }
+        }
+
+
+        return collection.length();
+    }
+}
+/*
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string.
+The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+Example:
+"abcde" -> 0 # no characters repeats more than once.
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times.
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice.
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice.
+ */
---
+++ src/main/java/org/example/jaden_casing_string/Main.java
@@ -0,0 +1,45 @@
+package org.example.jaden_casing_string;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "How can mirrors be real if our eyes aren't real";
+        System.out.println(jadenMethod(str));
+
+
+    }
+
+    private static String jadenMethod(String str) {
+        String res = "";
+
+        try {
+
+            String[] arr = str.split(" ");
+
+            for (String each : arr) {
+                res += each.replaceFirst(each.substring(0, 1), each.substring(0, 1).toUpperCase()) + " ";
+
+            }
+
+        } catch (Exception e) {
+            return " ";
+        }
+
+        return res.trim();
+    }
+}
+/*
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013).
+Jaden is also known for some of his philosophy that he delivers via Twitter.
+When writing on Twitter, he is known for almost always capitalizing every word.
+For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+
+Your task is to write a method that converts strings to how they would be written by Jaden Smith.
+The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+Note that this function should a return empty string for an empty string or null.
+
+Example:
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+ */
