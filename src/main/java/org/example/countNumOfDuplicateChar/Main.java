bf0971c1dc0d37f066ce250731d6fa6c91f13062
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-12 00:32:40 -0500

    Preparation to Interview week 04

--- src/main/java/org/example/countNumOfDuplicateChar/Main.java
+++ src/main/java/org/example/countNumOfDuplicateChar/Main.java
@@ -12,7 +12,6 @@ public class Main {
         System.out.println(countDuplicate2(str));
        // System.out.println(countDuplicate(str));
 
-
     }
     private static Integer countDuplicate2(String str){
 
