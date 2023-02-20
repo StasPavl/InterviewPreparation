0f82b11a04a0775b61794e43b27a69f1503988bc
stanislavpavlovskyi <lichfb@gmail.com>
2023-02-19 20:40:08 -0500

    Preparation to Interview week 06 Valid Palidrom task added

---
+++ src/main/java/org/example/validPalidrom/VP.java
@@ -0,0 +1,24 @@
+package org.example.validPalidrom;
+
+public class VP {
+    public static void main(String[] args) {
+//                    0
+        String str = "0P";
+        System.out.println(isPalidrome(str));
+
+    }
+    private static boolean isPalidrome(String str){
+        str = str.toLowerCase().replaceAll("[^a-z]","");
+        int n =str.length();
+        int i =0, j = n-1;
+
+        while (i<j){
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
