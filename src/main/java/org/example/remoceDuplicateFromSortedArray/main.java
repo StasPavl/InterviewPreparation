d3e7c0abb598e6d74ea1cb3893ddfc329163761c
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-06 18:07:36 -0500

    remove duplicates from sorted array

---
+++ src/main/java/org/example/remoceDuplicateFromSortedArray/main.java
@@ -0,0 +1,27 @@
+package org.example.remoceDuplicateFromSortedArray;
+
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    public int removeDuplicates(int[] nums) {
+
+        int i = 0;
+
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[j] != nums[i]) {
+                nums[++i] = nums[j];
+            }
+        }
+
+
+        int[] uniqueNums = Arrays.copyOf(nums, i + 1);
+        return uniqueNums.length;
+    }
+
+}
