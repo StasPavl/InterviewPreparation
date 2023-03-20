15a2fd96e43cbdf4e14d0488149eaf3b6e5936c7
stanislavpavlovskyi <lichfb@gmail.com>
2023-03-19 23:01:51 -0400

    Excel Sheet Column Number Task

---
+++ src/main/java/org/example/excelSheetColumnNumber/Main.java
@@ -0,0 +1,18 @@
+package org.example.excelSheetColumnNumber;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(titleToNumber("ZY"));
+
+    }
+    public static int titleToNumber(String columnTitle){
+        int result = 0;
+        //going through all chars in my string
+        for (int i = 0; i < columnTitle.length(); i++) {
+            char c = columnTitle.charAt(i);
+           int columnNum = c -'A' + 1; // checking what is number of my char (1-26)
+           result = result * 26 + columnNum;
+        }
+        return result;
+    }
+}
