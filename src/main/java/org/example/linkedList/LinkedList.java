831d0ec0ab2b5d17d3589bef439ca85488cf1133
stanislavpavlovskyi <lichfb@gmail.com>
2023-10-11 11:06:02 -0400

    Linked List interview prep

--- src/main/java/org/example/linkedList/LinkedList.java
+++ src/main/java/org/example/linkedList/LinkedList.java
@@ -148,4 +148,32 @@ public class LinkedList {
         length++;
         return true;
     }
+    public Node remove(int index){
+        if (index < 0 || index >= length) return null;
+        if ( index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+        Node prev = get(index-1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+    public void reverse (){
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        Node after = temp.next;
+        Node before = null;
+
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+
+    }
 }
